9ebe2416c20d4fccdecb30df3b9fd696e110e7c5
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-04-01 09:16:04 -1000

    adds in break and now duplicated numbers work too

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var messyArr = [3,1,5,8,4,2];
+var messyArr = [3,1,5,8,4,4,4,4,4,3,2];
 
 
 function insertionSort(arr) {
@@ -6,30 +6,27 @@ function insertionSort(arr) {
   var unsorted = arr;
   sorted.push(arr[0]);
   arr.shift();
-  console.log('sorted is: ', sorted);
-  console.log('unsorted is: ', unsorted);
 
-  for(var i = 0; i < 5; i++) {
+
+  for(var i = 0; i < arr.length; i++) {
   var c = sorted.length;
     for(var j = 0; j < c; j++) {
 
-      if(unsorted[i] > sorted[j] && unsorted[i]< sorted[j+1]) {
-        console.log(j);
+      if(unsorted[i] > sorted[j] && unsorted[i]<= sorted[j+1]) {
         sorted.splice((j+1), 0, unsorted[i]);
-        console.log('here', sorted);
+        break;
       } else if (unsorted[i] < sorted[0]){
         sorted.unshift(unsorted[i]);
-        console.log('here', sorted);
       } else if (unsorted[i] > sorted[sorted.length-1]){
         sorted.push(unsorted[i]);
+        break;
 
-        console.log('here', sorted);
       }
     }
   } //end of big for
-  console.log(sorted);
+
   return sorted;
 }
 
-insertionSort(messyArr);
+console.log('Answer: ', insertionSort(messyArr));
 // module.exports = insertionSort;
